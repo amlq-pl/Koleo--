9ec00cc3b13002f21399913c5988f91391b1342b
phofis <pasula.piotr@gmail.com>
2024-04-30 13:20:39 +0200

    updated databse

---
+++ src/main/resources/database/data_copy.sql
@@ -0,0 +1,10 @@
+copy szczegoly_biletu(id_szczegolow, rower, dodatkowy_bagaz, zwierze) from stdin;
+0   false   false   false
+1   true   false   false
+2   false   true   false
+3   true   true   false
+4   false   false   true
+5   true   false   true
+6   false   true   true
+7   true   true   true
+\.
