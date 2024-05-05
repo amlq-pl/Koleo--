d21f22499ccb3045334128399c20d59f0f24f155
phofis <pasula.piotr@gmail.com>
2024-05-05 23:09:29 +0200

    added more data generators

--- src/main/resources/database/data_generators/konto_gen.py
+++ src/main/resources/database/data_generators/konto_gen.py
@@ -4,12 +4,14 @@ import string
 
 import params
 
+
 def fetch_random_login():
-    str=""
-    l=random.randint(8,20)
+    str = ""
+    l = random.randint(8, 20)
     for i in range(l):
-        str+=random.choice(string.ascii_letters)
+        str += random.choice(string.ascii_letters)
     return str
+
 
 script_path = os.path.dirname(__file__)
 script_path = script_path[:-16]
@@ -17,11 +19,11 @@ script_path += "/data/konto.sql"
 
 file = open(script_path, 'w')
 file.write("copy konto(id_konta, login, haslo, id_klienta) from stdin delimiter ',';\n")
-counter=0
+counter = 0
 for i in range(round(params.general_size / 100)):
-    if(5<random.randint(1,6)):
+    if (5 < random.randint(1, 6)):
         continue
     str = f"""{counter},{fetch_random_login()},{-1480617004},{i}\n"""
     file.write(str)
-    counter+=1
+    counter += 1
 file.close()
