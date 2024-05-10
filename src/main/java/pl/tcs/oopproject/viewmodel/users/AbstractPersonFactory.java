94f87a7bff833b0a06b77863d29f5738bf1bd2c5
arendaczh <arendaczh@gmail.com>
2024-05-10 15:57:34 +0200

    poprawione person class

---
+++ src/main/java/pl/tcs/oopproject/viewmodel/users/AbstractPersonFactory.java
@@ -0,0 +1,5 @@
+package pl.tcs.oopproject.viewmodel.users;
+
+public class AbstractPersonFactory {
+	public Person create;
+}
--- src/main/java/pl/tcs/oopproject/viewmodel/users/Person.java
+++ src/main/java/pl/tcs/oopproject/viewmodel/users/Person.java
@@ -4,26 +4,41 @@ import pl.tcs.oopproject.viewmodel.findconnection.KoleoException;
 
 import java.time.LocalDate;
 
-public class Person extends User {
-	String name;
-	String surname;
-	LocalDate dateOfBirth;
-	String emailAddress;
-	String telephoneNumber;
+public class Person {
+	private String name;
+	private String surname;
+	private LocalDate dateOfBirth;
+	private String emailAddress;
+	private String telephoneNumber = null;
 	
-	public Person(String name, String surname, LocalDate dateOfBirth, String emailAdress, String login, String password) throws KoleoException {
-		super(login, password);
+	public Person(String name, String surname, LocalDate dateOfBirth, String emailAdress) throws KoleoException {
 		this.name = name;
 		this.surname = surname;
 		this.dateOfBirth = dateOfBirth;
 		this.emailAddress = emailAdress;
-		//TU NASTĘPUJE CHECK CZY DANE OSOBOWE SĄ POPRAWNE ORAZ WYWOŁANIE ZAPISU DANEJ OSOBY DO BAZY
 	}
-	public Person(String name, String surname, LocalDate dateOfBirth, String emailAddress, String telephoneNumber, String login, String password) throws KoleoException{
-		this(name, surname, dateOfBirth, emailAddress, login, password);
+	public Person(String name, String surname, LocalDate dateOfBirth, String emailAddress, String telephoneNumber) throws KoleoException{
+		this(name, surname, dateOfBirth, emailAddress);
 		this.telephoneNumber = telephoneNumber;
 	}
 	
+	public String getName() {
+		return name;
+	}
 	
+	public String getSurname() {
+		return surname;
+	}
 	
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	
+	public String getTelephoneNumber() {
+		return telephoneNumber;
+	}
 }
