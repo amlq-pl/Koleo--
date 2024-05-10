2d5b5881e39870d456365bcd6381a6d6f19c19b6
arendaczh <arendaczh@gmail.com>
2024-05-10 18:27:00 +0200

    adding Person class, waiting for static methods to save data to database

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+import pl.tcs.oopproject.viewmodel.exception.KoleoException;
 import pl.tcs.oopproject.viewmodel.users.Person;
 import pl.tcs.oopproject.viewmodel.users.PersonFactory;
 
@@ -7,19 +8,14 @@ import java.time.LocalDate;
 import static org.junit.Assert.*;
 
 public class PersonTest {
-//	@Test
-//	public void dateAndPhoneNumberTest() {
-//		Person person = new Person("Helena", "Arendacz", LocalDate.of(2005, 1, 6), "arendaczh@gmail.com", "+48606544481");
-//		assertTrue(PersonFactory.correctEmail(person.getEmailAddress()));
-//		person.setEmailAddress("arendacz.helenaxd");
-//		assertFalse(PersonFactory.correctEmail(person.getEmailAddress()));
-//		person.setEmailAddress("xd.@gmail");
-//		assertFalse(PersonFactory.correctEmail(person.getEmailAddress()));
-//		assertTrue(PersonFactory.correctTelephoneNumber(person.getTelephoneNumber()));
-//		person.setTelephoneNumber("606544481");
-//		assertFalse(PersonFactory.correctTelephoneNumber(person.getTelephoneNumber()));
-//		person.setTelephoneNumber("+16 0654 44 8 1");
-//		assertTrue(PersonFactory.correctTelephoneNumber(person.getTelephoneNumber()));
-//
-//	}
+	@Test
+	public void dateAndPhoneNumberTest() {
+		try {
+			Person A = new Person("Aaaa", "Bbbb", LocalDate.of(2000, 12, 13), "sfnio234f@student.uj.edu.pl", null);
+		}
+		catch(KoleoException e) {
+			System.out.println("oo");
+		}
+
+	}
 }
