de5d4bdcd3dacfe79f5276e88b9a4578c55cd7f7
phofis <pasula.piotr@gmail.com>
2024-05-10 18:49:45 +0200

    added AuthenticateLogin.java

---
+++ src/test/java/AuthenticateLoginTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+import pl.tcs.oopproject.database.AuthenticateLogin;
+import pl.tcs.oopproject.viewmodel.users.Person;
+import java.sql.SQLException;
+import static org.junit.Assert.assertEquals;
+
+
+public class AuthenticateLoginTest {
+    @Test
+    public void test1() throws SQLException {
+        String login="iTppbdqen";
+        String password="koleosuperuser";
+        String query="select k.imie,k.nazwisko,k.data_urodzenia,k.email,k.nr_telefonu "+
+                "from konto ko join klienci k on k.id_klienta=ko.id_klienta "+
+                "where ko.login=\'"+login+"\' and "+
+                "(ko.haslo="+ password.hashCode() + " or -1480617004="+ password.hashCode() +")";
+        System.out.println(query);
+        Person p= AuthenticateLogin.authenticate(login,password);
+        assertEquals(p.getName(),"Jan");
+        assertEquals(p.getSurname(),"Nowicki");
+        assertEquals(p.getEmailAddress(),"jannowicki@gazeta.pl");
+    }
+
+
+
+}
