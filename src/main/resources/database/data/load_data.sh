eeb184a1887da577e8ede665a52d5f29dd889a99
phofis <pasula.piotr@gmail.com>
2024-05-10 19:02:30 +0200

    readded load_data.sh

---
+++ src/main/resources/database/data/load_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export PGPASSWORD="koleo"
+psql -h 127.0.0.1  -U "koleo_user" -d "koleo_db" -f "klienci.sql"
+psql -h 127.0.0.1  -U "koleo_user" -d "koleo_db" -f "konto.sql"
+psql -h 127.0.0.1  -U "koleo_user" -d "koleo_db" -f "przewoznicy.sql"
+psql -h 127.0.0.1  -U "koleo_user" -d "koleo_db" -f "ulgi.sql"
+psql -h 127.0.0.1  -U "koleo_user" -d "koleo_db" -f "rabaty.sql"
+psql -h 127.0.0.1  -U "koleo_user" -d "koleo_db" -f "szczegoly_biletu.sql"
+psql -h 127.0.0.1  -U "koleo_user" -d "koleo_db" -f "wagony.sql"
+psql -h 127.0.0.1  -U "koleo_user" -d "koleo_db" -f "wagony_typy_miejsc.sql"
+
+unset PGPASSWORD
+
