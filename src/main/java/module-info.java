db828663dffa7aa8951fd3e0d0f73fb5f9c28192
phofis <pasula.piotr@gmail.com>
2024-05-10 16:04:22 +0200

    started work on database connectors

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,4 +4,6 @@ module Koleo {
     requires java.sql;
     opens pl.tcs.oopproject to javafx.fxml;
     exports pl.tcs.oopproject;
+    exports pl.tcs.oopproject.database;
+    opens pl.tcs.oopproject.database to javafx.fxml;
 }
---
+++ src/main/java/pl/tcs/oopproject/database/AuthenticateLogin.java
@@ -0,0 +1,13 @@
+package pl.tcs.oopproject.database;
+
+import pl.tcs.oopproject.viewmodel.users.*;
+
+import java.sql.Statement;
+
+
+public class AuthenticateLogin {
+    public static Person authenticate(String login, String password){
+        Statement statement=DB.connection;
+        statement.execute()
+    }
+}
