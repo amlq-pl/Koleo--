a9f3a37b89df61773bd8edf95eae4dafa578feaf
jakub_janor_linux <kuba.janor@gmail.com>
2024-05-11 14:30:49 +0200

    Added Landing Scene

--- src/main/java/pl/tcs/oopproject/view/sceneControllers/LandingSceneController.java
+++ src/main/java/pl/tcs/oopproject/view/sceneControllers/LandingSceneController.java
@@ -1,4 +1,18 @@
 package pl.tcs.oopproject.view.sceneControllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class LandingSceneController {
+
+    public Button SignUpButton;
+    public Button LogInButton;
+
+    @FXML
+    protected void SignUpClicked() {
+    }
+
+    @FXML
+    protected void LogInClicked() {
+    }
 }
