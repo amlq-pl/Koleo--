f35fe8b2cdb7859625c0e505bdf872a67ebe4e6b
jakub_janor_linux <kuba.janor@gmail.com>
2024-05-11 12:09:22 +0200

    Added javafx dependencies

--- src/main/java/pl/tcs/oopproject/App.java
+++ src/main/java/pl/tcs/oopproject/App.java
@@ -11,7 +11,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("scenes/landing-scene.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("scenes/landing-scene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 480, 320);
         stage.setTitle("Main app");
         stage.setScene(scene);
--- src/main/java/pl/tcs/oopproject/view/sceneControllers/LandingSceneController.java
+++ src/main/java/pl/tcs/oopproject/view/sceneControllers/LandingSceneController.java
@@ -1,20 +1,31 @@
 package pl.tcs.oopproject.view.sceneControllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Border;
 import pl.tcs.oopproject.viewmodel.users.Person;
 import pl.tcs.oopproject.viewmodel.users.PersonFactory;
 
+import java.net.URL;
 import java.time.LocalDate;
+import java.util.Date;
+import java.util.ResourceBundle;
 
-public class LandingSceneController {
+public class LandingSceneController implements Initializable {
     public TextField NameTextField;
     public TextField SurnameTextField;
     public DatePicker BirthDatePicker;
     public TextField EmailTextField;
     public TextField PhoneNumberTextField;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        LocalDate date = LocalDate.now();
+        BirthDatePicker.setValue(date);
+    }
     @FXML
     protected void onHelloButtonClick() {
         String name = NameTextField.getText();
@@ -23,7 +34,26 @@ public class LandingSceneController {
         String emailAddress = EmailTextField.getText();
         String phoneNumber = PhoneNumberTextField.getText();
 
-        PersonFactory factory = new PersonFactory();
+        if (name.isEmpty()) {
+            NameTextField.setStyle("-fx-background-color: red");
+        }
+        if (surname.isEmpty()) {
+            SurnameTextField.setStyle("-fx-background-color: red");
+        }
+        else {
+            PersonFactory factory = new PersonFactory();
+        }
 //        Person p = factory.create();
     }
+
+    public void onNameClicked() {
+        NameTextField.setStyle("-fx-background-color: white");
+        SurnameTextField.setStyle("-fx-background-color: white");
+    }
+
+    public void onSurnameClicked() {
+        NameTextField.setStyle("-fx-background-color: white");
+        SurnameTextField.setStyle("-fx-background-color: white");
+    }
+
 }
