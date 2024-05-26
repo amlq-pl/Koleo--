8a9ec3b7167f0f1a489e08fdac22a2aa550ba095
phofis <pasula.piotr@gmail.com>
2024-05-26 20:50:26 +0200

    new class FindPlacesForConnectionWithTranfers.java

---
+++ src/main/java/pl/tcs/oopproject/postgresDatabaseIntegration/FindPlacesForConnectionWithTranfers.java
@@ -0,0 +1,12 @@
+package pl.tcs.oopproject.postgresDatabaseIntegration;
+
+import pl.tcs.oopproject.model.databaseIntegration.FindPlacesForConnectionWithTransfersInterface;
+import pl.tcs.oopproject.viewmodel.connection.ConnectionWithTransfers;
+import pl.tcs.oopproject.viewmodel.place.Place;
+
+public class FindPlacesForConnectionWithTranfers implements FindPlacesForConnectionWithTransfersInterface {
+    @Override
+    public Place findPlacesForConnectionWithTransfers(ConnectionWithTransfers connectionWithTransfers) {
+        return null;
+    }
+}
