20911e3776a57b3c2ae36df21a3444607d0e214b
jakub_janor_linux <kuba.janor@gmail.com>
2024-05-26 13:53:21 +0200

    added hour selector

--- src/main/java/pl/tcs/oopproject/view/sceneControllers/TrainSearchSceneController.java
+++ src/main/java/pl/tcs/oopproject/view/sceneControllers/TrainSearchSceneController.java
@@ -3,11 +3,20 @@ package pl.tcs.oopproject.view.sceneControllers;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.converter.LocalDateTimeStringConverter;
 import pl.tcs.oopproject.view.ViewController;
 import pl.tcs.oopproject.view.componentControllers.TrainPane;
 import pl.tcs.oopproject.viewmodel.connection.ConnectionFinder;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class TrainSearchSceneController {
 
@@ -15,16 +24,17 @@ public class TrainSearchSceneController {
     public Button ExitButton;
     public Button ConfirmButton;
     public VBox ScrollablePane;
-    private static ConnectionFinder finder;
-
-//    {
-//        finder = new ConnectionFinder();
-//    }
 
     private final ObservableList<TrainPane> ConnectionList = FXCollections.observableArrayList();
+    public TextField DepStation;
+    public TextField ArrStation;
+    public DatePicker ConnectionDate;
+    private final ArrayList<String> Hours = new ArrayList<>(List.of("a", "b", "c"));
+    public ComboBox HourPicker = new ComboBox(FXCollections.observableList(Hours));
 
     public void ConfirmButtonClick()  {
         // TODO: add validation using validator FX or something like this
+//        ConnectionFinder finder =  new ConnectionFinder(DepStation.textProperty(), ArrStation.textProperty());
         System.out.println("Hello yo");
         TrainPane pane = new TrainPane();
         ConnectionList.add(pane);
