142a1ad7fd25f4c752473a8c87011b0c5e5eb396
phofis <pasula.piotr@gmail.com>
2024-05-26 23:29:32 +0200

    started working on FindPlacesForConnectionWithTransfers

--- src/main/java/pl/tcs/oopproject/postgresDatabaseIntegration/FindPlacesForConnectionWithTransfers.java
+++ src/main/java/pl/tcs/oopproject/postgresDatabaseIntegration/FindPlacesForConnectionWithTransfers.java
@@ -2,11 +2,24 @@ package pl.tcs.oopproject.postgresDatabaseIntegration;
 
 import pl.tcs.oopproject.model.databaseIntegration.FindPlacesForConnectionWithTransfersInterface;
 import pl.tcs.oopproject.viewmodel.connection.ConnectionWithTransfers;
+import pl.tcs.oopproject.viewmodel.connection.DirectConnection;
 import pl.tcs.oopproject.viewmodel.place.Place;
+import pl.tcs.oopproject.viewmodel.place.SpecificSeat;
+
+import java.util.ArrayList;
 
 public class FindPlacesForConnectionWithTransfers implements FindPlacesForConnectionWithTransfersInterface {
     @Override
     public Place findPlacesForConnectionWithTransfers(ConnectionWithTransfers connectionWithTransfers) {
+        ArrayList<SpecificSeat> places = new ArrayList<>();
+        for (DirectConnection directConnection : connectionWithTransfers.getTrains()) {
+            places.add(findSpecificSeat(directConnection));
+        }
+        return new Place(connectionWithTransfers, places);
+    }
+
+    private SpecificSeat findSpecificSeat(DirectConnection directConnection) {
         return null;
+
     }
 }
