3648bc204de201f5f83a9b28572ecf03c83d7d5b
jakub_janor_linux <kuba.janor@gmail.com>
2024-05-27 05:38:55 +0200

    Added Basket class

---
+++ src/main/java/pl/tcs/oopproject/view/Basket.java
@@ -0,0 +1,28 @@
+package pl.tcs.oopproject.view;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import pl.tcs.oopproject.viewmodel.connection.ConnectionWithTransfers;
+
+public class Basket {
+    public ObservableList<ConnectionWithTransfers> itemsList = FXCollections.observableArrayList();
+    public IntegerProperty size = new SimpleIntegerProperty();
+
+    {
+        itemsList.addListener((ListChangeListener<? super ConnectionWithTransfers>) change -> {
+            while (change.next()) {
+                if (change.wasAdded()) {
+                    size.setValue(size.getValue() + 1);
+                }
+                if (change.wasRemoved()) {
+                    size.setValue(size.getValue() - 1);
+                }
+            }
+        });
+    }
+
+    public Basket() {}
+}
