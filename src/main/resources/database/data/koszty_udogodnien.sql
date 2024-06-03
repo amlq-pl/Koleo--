7d8b8ebe7f9ce88d45a408a3d24f114929e21761
phofis <pasula.piotr@gmail.com>
2024-06-03 23:55:41 +0200

    modifying sql table structure

---
+++ src/main/resources/database/data/koszty_udogodnien.sql
@@ -0,0 +1,4 @@
+copy koszty_udogodnien(nazwa,koszt_procentowy,data_poczatkowa,data_koncowa) from stdin delimiter ',';
+dodatkowy bagaż,7,2024-01-01,2025-01-01
+przewóz zwierzęcia,11,2024-01-01,2025-01-01
+przewóz roweru,13,2024-01-01,2025-01-01
--- src/main/resources/database/data_generators/zamowienia_gen.py
+++ src/main/resources/database/data_generators/zamowienia_gen.py
@@ -1,6 +1,23 @@
 import os
 import random
+
+import dateutil.relativedelta
+
 import params
+import datetime as dt
+from datetime import datetime
+
+def fetch_random_timestamp(date_format='%Y-%m-%d') -> str:
+    start = datetime.strptime(params.start_symulacji, date_format)
+    end = datetime.strptime(params.koniec_symulacji, date_format)
+    start -= dateutil.relativedelta.relativedelta(months=1)
+    end -= dateutil.relativedelta.relativedelta(months=1)
+
+    delta = end - start
+    random_days = random.randint(0, delta.days)
+    random_date = start + dt.timedelta(days=random_days)
+
+    return random_date.strftime(date_format) + f' {random.randrange(24)}:{random.randrange(60)}:00.000000'
 
 script_path = os.path.dirname(__file__)
 script_path = script_path[:-16]
@@ -9,5 +26,5 @@ script_path += "/data/zamowienia.sql"
 file = open(script_path, 'w')
 file.write("copy zamowienia(id_klienta) from stdin delimiter ',';\n")
 for i in range(params.liczba_zamowien):
-    file.write(f"""{random.randint(1, params.liczba_klientow)}\n""")
+    file.write(f"""{random.randint(1, params.liczba_klientow)},{fetch_random_timestamp()}\n""")
 file.close()
