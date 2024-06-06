bf87597ffb3e65bc5aaa05abfea41e2ade9a0dee
jakub_janor_linux <kuba.janor@gmail.com>
2024-06-06 23:36:16 +0200

    modified CheckersInterface

--- src/main/java/pl/tcs/oopproject/model/databaseIntegration/CheckersInterface.java
+++ src/main/java/pl/tcs/oopproject/model/databaseIntegration/CheckersInterface.java
@@ -1,5 +1,8 @@
 package pl.tcs.oopproject.model.databaseIntegration;
 
+import pl.tcs.oopproject.model.discount.Discount;
+import pl.tcs.oopproject.model.discount.Voucher;
+
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -7,4 +10,6 @@ public interface CheckersInterface {
 	boolean checkIfUserExists(String login) throws SQLException;
 	
 	ArrayList<String> getAllStations() throws SQLException;
+	ArrayList<Discount> getAllDiscounts() throws SQLException;
+	ArrayList<Voucher> getAllVouchers() throws SQLException;
 }
