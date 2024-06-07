1aaa2d74f7bfae887ccdf8546d1838c6ac7a3a55
arendaczh <arendaczh@gmail.com>
2024-06-07 20:34:04 +0200

    long term ticket interface

--- src/main/java/pl/tcs/oopproject/model/databaseIntegration/CreateOrRefactorTicket.java
+++ src/main/java/pl/tcs/oopproject/model/databaseIntegration/CreateOrRefactorTicket.java
@@ -1,11 +1,11 @@
 package pl.tcs.oopproject.model.databaseIntegration;
 
-import pl.tcs.oopproject.model.connection.MultiStopRoute;
 import pl.tcs.oopproject.model.discount.Discount;
 import pl.tcs.oopproject.model.discount.Voucher;
 import pl.tcs.oopproject.model.place.TrainsAssignedSeats;
 import pl.tcs.oopproject.model.ticket.Details;
 import pl.tcs.oopproject.model.ticket.LongTermTicketType;
+import pl.tcs.oopproject.model.ticket.LongTermTrainTicket;
 import pl.tcs.oopproject.model.ticket.SingleJourneyTrainTicket;
 import pl.tcs.oopproject.model.users.Person;
 
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 
 public interface CreateOrRefactorTicket {
 	// RETURN ID OF THE TICKET
-	int saveLongTermTicket(LocalDateTime startDate, Discount discount, Voucher voucher, String login, LongTermTicketType ticketType, Person person);
+	ArrayList<LongTermTrainTicket> saveLongTermTicket(LocalDateTime startDate, Discount discount, Voucher voucher, String login, LongTermTicketType ticketType, Person person);
 	
 	/**
 	 * the method has to return ArrayList of SingleUseTickets (Person, voucher, discount, details, ArrayList<TrainsAssignedSeats>  (each seat has getTrain)
