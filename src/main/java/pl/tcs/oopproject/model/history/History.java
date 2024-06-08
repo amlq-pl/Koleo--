ef0ddf211d684a0acc7dbe925a256d7fc39dcf12
arendaczh <arendaczh@gmail.com>
2024-06-08 14:31:45 +0200

    tickets - minor changes

--- src/main/java/pl/tcs/oopproject/model/history/History.java
+++ src/main/java/pl/tcs/oopproject/model/history/History.java
@@ -1,28 +1,50 @@
 package pl.tcs.oopproject.model.history;
 
+import pl.tcs.oopproject.model.ticket.LongTermTrainTicket;
+import pl.tcs.oopproject.model.ticket.SingleJourneyTrainTicket;
 import pl.tcs.oopproject.model.ticket.TrainTicket;
 
 import java.util.ArrayList;
 
 public class History {
-	private static ArrayList<TrainTicket> tickets;
+	private  ArrayList<SingleJourneyTrainTicket> singleJourneyTickets;
+	private ArrayList<LongTermTrainTicket> longTermTickets;
 	
 	private void setData() {
 		//SET DATA ABOUT TICKETS
+		//
 	}
 	
-	public ArrayList<TrainTicket> activeTickets(){
-		ArrayList<TrainTicket> activeTickets = new ArrayList<>();
-		for(TrainTicket t : tickets) {
+	public ArrayList<LongTermTrainTicket> activeLongTermTickets(){
+		ArrayList<LongTermTrainTicket> activeTickets = new ArrayList<>();
+		for(LongTermTrainTicket t : longTermTickets) {
 			if(t.isActive())
 				activeTickets.add(t);
 		}
 		return activeTickets;
 	}
 	
-	public ArrayList<TrainTicket> archivedTickets() {
-		ArrayList<TrainTicket> archivedTickets = new ArrayList<>();
-		for(TrainTicket t : tickets) {
+	public ArrayList<SingleJourneyTrainTicket> activeSingleJourneyTickets(){
+		ArrayList<SingleJourneyTrainTicket> activeTickets = new ArrayList<>();
+		for(SingleJourneyTrainTicket t : singleJourneyTickets) {
+			if(t.isActive())
+				activeTickets.add(t);
+		}
+		return activeTickets;
+	}
+	
+	public ArrayList<SingleJourneyTrainTicket> archivedSingleJourneyTickets() {
+		ArrayList<SingleJourneyTrainTicket> archivedTickets = new ArrayList<>();
+		for(SingleJourneyTrainTicket t : singleJourneyTickets) {
+			if(!t.isActive())
+				archivedTickets.add(t);
+		}
+		return archivedTickets;
+	}
+	
+	public ArrayList<LongTermTrainTicket> archivedLongTermTickets() {
+		ArrayList<LongTermTrainTicket> archivedTickets = new ArrayList<>();
+		for(LongTermTrainTicket t : longTermTickets) {
 			if(!t.isActive())
 				archivedTickets.add(t);
 		}
