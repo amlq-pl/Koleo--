60b31032cd18a891b9eb47cf7f4b5cfdfc6544f8
jakub_janor_linux <kuba.janor@gmail.com>
2024-06-08 17:25:41 +0200

    jeżyk

--- src/main/java/pl/tcs/oopproject/App.java
+++ src/main/java/pl/tcs/oopproject/App.java
@@ -2,6 +2,8 @@ package pl.tcs.oopproject;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import pl.tcs.oopproject.model.discount.Discount;
+import pl.tcs.oopproject.model.discount.Voucher;
 import pl.tcs.oopproject.postgresDatabaseIntegration.Checkers;
 import pl.tcs.oopproject.view.Basket;
 import pl.tcs.oopproject.view.ViewController;
@@ -11,13 +13,16 @@ import java.util.ArrayList;
 
 public class App extends Application {
 	public static ArrayList<String> Stations;
+	public static ArrayList<Discount> Discounts;
+	public static ArrayList<Voucher> Vouchers;
 	private final Checkers checkers = new Checkers();
 
 	public static Basket basket = new Basket();
-	
 	{
 		try {
 			Stations = checkers.getAllStations();
+			Discounts = checkers.getAllDiscounts();
+			Vouchers = checkers.getAllVouchers();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
