2032cd47766912fa1ee0c0b74b4c517a1265f665
arendaczh <arendaczh@gmail.com>
2024-06-08 17:44:52 +0200

    additions

--- src/main/java/pl/tcs/oopproject/model/ticket/Addition.java
+++ src/main/java/pl/tcs/oopproject/model/ticket/Addition.java
@@ -1,4 +1,19 @@
 package pl.tcs.oopproject.model.ticket;
 
-public record Addition(String name, double cost, boolean active) {
+public class Addition {
+	private final String name;
+	private final double cost;
+	private boolean active = false;
+	
+	Addition(String name, double cost) {
+		this.name = name;
+		this.cost = cost;
+	}
+	
+	Addition(Addition addition, boolean active) {
+		this.name = addition.name;
+		this.cost = addition.cost;
+		this.active = active;
+	}
+	
 }
