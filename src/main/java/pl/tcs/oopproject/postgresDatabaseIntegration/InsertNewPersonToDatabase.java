6147ffc02880edbfa48046282797e295b933d4e9
phofis <pasula.piotr@gmail.com>
2024-06-09 06:13:03 +0200

    .

--- src/main/java/pl/tcs/oopproject/postgresDatabaseIntegration/InsertNewPersonToDatabase.java
+++ src/main/java/pl/tcs/oopproject/postgresDatabaseIntegration/InsertNewPersonToDatabase.java
@@ -3,6 +3,7 @@ package pl.tcs.oopproject.postgresDatabaseIntegration;
 import pl.tcs.oopproject.model.databaseIntegration.InsertNewPersonToDatabaseInterface;
 import pl.tcs.oopproject.model.users.Person;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
@@ -13,7 +14,7 @@ public class InsertNewPersonToDatabase implements InsertNewPersonToDatabaseInter
         PreparedStatement ps = DB.connection.prepareStatement("insert into uzytkownicy(imie, nazwisko, data_urodzenia, email, nr_telefonu, login, haslo) values(?, ?, ?, ?, ?, ?, ?);");
         ps.setString(1, p.getName());
         ps.setString(2, p.getSurname());
-        ps.setString(3, p.getDateOfBirth().toString());
+        ps.setDate(3, Date.valueOf(p.getDateOfBirth()));
         ps.setString(4, p.getEmailAddress());
         ps.setString(5, p.getTelephoneNumber());
         ps.setString(6, login);
