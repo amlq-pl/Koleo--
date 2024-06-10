4cf79b272615894f9cf4977e50ff9bce6189100f
phofis <pasula.piotr@gmail.com>
2024-06-10 12:08:41 +0200

    .

--- src/main/resources/database/create.sh
+++ src/main/resources/database/create.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 sudo -u postgres psql -c "SELECT pg_terminate_backend(pg_stat_activity.pid)
 FROM pg_stat_activity
 WHERE pg_stat_activity.datname = 'koleo_db'
--- src/main/resources/database/data_generators/gen_data.sh
+++ src/main/resources/database/data_generators/gen_data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 python3 konto_gen.py
 python3 klienci_gen.py
 python3 wagony_gen.py
